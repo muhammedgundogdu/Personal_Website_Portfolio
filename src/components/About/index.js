8988fde3147686c128a3d7fb4f1098a41d8cc8c0
muhammedgundogdu <muhammedgundogdu45@gmail.com>
2022-10-03 15:04:23 +0300

    Finish
    .

---
+++ src/components/About/index.js
@@ -0,0 +1,90 @@
+import { useEffect, useState } from 'react'
+import {
+  faAngular,
+  faCss3,
+  faGitAlt,
+  faGithub,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './index.scss'
+
+const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+
+
+  const getPosts = async () => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+  //   useEffect(() => {
+  //     return setTimeout(() => {
+  //       setLetterClass('text-animate-hover')
+  //     }, 3000)
+  //   }, [])
+
+  return (
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            Merhaba, ben Javascript, React ve Frontend teknolojileri ile çalışmaya meraklı, kod yazmaya hırslı bir developer'ım.
+            <br />
+            <br />
+            Hello, I'm an ambitious coding developer who is interested in working with Javascript, React and Frontend technologies.
+          </p>
+          <p align="LEFT">
+            Günden güne kendime yeni bir şeyler katmak, yeni teknolojiler öğrenmek için çok çalışıyorum.
+            <br />
+            <br />
+            I work hard every day to add something new to myself and learn new technologies.
+          </p>
+
+        </div>
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faGithub} color="#000000" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default About
